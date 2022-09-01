0925cd8e8d8cab5c38221ae04950d4b247553f06
Robin <robin.james.guerrier@gmail.com>
2022-09-01 17:18:52 -0400

    Rework button node selectors to select all. Add entry and equation array building functionality via button inputs. Add clear button functionality.

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const display = document.querySelector(".display");
+let entryArray = [];
+let numberEntry = "";
 
-const oneButton = document.querySelector(".one");
-const twoButton = document.querySelector(".two");
-const threeButton = document.querySelector(".three");
-const fourButton = document.querySelector(".four");
-const fiveButton = document.querySelector(".five");
-const sixButton = document.querySelector(".six");
-const sevenButton = document.querySelector(".seven");
-const eightButton = document.querySelector(".eight");
-const nineButton = document.querySelector(".nine");
-const zeroButton = document.querySelector(".zero");
+const displayEquation = document.querySelector(".equation");
+const displayEntry = document.querySelector(".entry");
 
-const addButton = document.querySelector(".add");
-const subtractButton = document.querySelector(".subtract");
-const multiplyButton = document.querySelector(".multiply");
-const divideButton = document.querySelector(".divide");
-const operateButton = document.querySelector(".operate");
+const numberButtons = document.querySelectorAll("button.number");
+const operatorButtons = document.querySelectorAll("button.operator");
 const clearButton = document.querySelector(".clear");
+
+numberButtons.forEach(button => button.addEventListener("click", (e) => {
+    numberEntry += button.textContent;
+    displayEntry.textContent = numberEntry;
+}));
+
+operatorButtons.forEach(button => button.addEventListener("click", (e) => {
+    if (numberEntry === "") {
+        return;
+    } else {
+        entryArray.push(numberEntry);
+        numberEntry = "";
+        entryArray.push(button.textContent);
+
+        displayEquation.textContent = entryArray.join(" ");
+        displayEntry.textContent = ">"
+    }
+}));
+
+clearButton.addEventListener("click", (e) => {
+    entryArray = [];
+    numberEntry = "";
+    displayEquation.textContent = ">";
+    displayEntry.textContent = ">";
+});
 
 // ---------------------- Math Functions ----------------------
 
@@ -37,3 +52,18 @@ function divide(x, y) {
 }
 
 // ------------------------------------------------------------
+
+
+
+/* 
+pressing the operate button:
+First, while requiring that the current entry is not blank,
+add the current remaining number entry to the array.
+Then take the entry array and loop through it in four stages, 
+i.e. for each operator accoring to the order of operations.
+In each stage, the array is looped through until an operator of that stage is found, 
+and then a pair is made from the pervious and subsequent index points.
+The pair is operated on according to the operator, 
+and the three items in the array are replaced with the result.
+The loop then continues until there are no more operators / there is only one item.
+*/
