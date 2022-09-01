446e924e6ed5e6200b204cecc5da5be5fc27edec
Robin <robin.james.guerrier@gmail.com>
2022-09-01 14:10:12 -0400

    Add basic math functions and variables

---
+++ index.js
@@ -0,0 +1,39 @@
+const display = document.querySelector(".display");
+
+const oneButton = document.querySelector(".one");
+const twoButton = document.querySelector(".two");
+const threeButton = document.querySelector(".three");
+const fourButton = document.querySelector(".four");
+const fiveButton = document.querySelector(".five");
+const sixButton = document.querySelector(".six");
+const sevenButton = document.querySelector(".seven");
+const eightButton = document.querySelector(".eight");
+const nineButton = document.querySelector(".nine");
+const zeroButton = document.querySelector(".zero");
+
+const addButton = document.querySelector(".add");
+const subtractButton = document.querySelector(".subtract");
+const multiplyButton = document.querySelector(".multiply");
+const divideButton = document.querySelector(".divide");
+const operateButton = document.querySelector(".operate");
+const clearButton = document.querySelector(".clear");
+
+// ---------------------- Math Functions ----------------------
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+// ------------------------------------------------------------
